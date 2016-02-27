e495c002d22cc3c2b56204c00f31ebad79e79b0a
= <=>
2016-02-27 18:08:38 +0000

    impletement edit and delete behaviour

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -13,16 +13,53 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
     // MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-
     @IBOutlet weak var photoImaageView: UIImageView!
-    
     @IBOutlet weak var ratingControl: RatingControl!
     
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    /*
+    This value is either passed by `MealTableViewController` in `prepareForSegue(_:sender:)`
+    or constructed as part of adding a new meal.
+    */
+    var meal: Meal?
+    
+    // MARK: Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = nameTextField.text ?? ""
+            let photo = photoImaageView.image
+            let rating = ratingControl.rating
+            
+            meal = Meal(name: name, photo: photo, rating: rating)
+        }
+        
+    }
+    
+    
     // MARK: Actions
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
+            dismissViewControllerAnimated(true, completion: nil)
+        } else {
+            navigationController!.popViewControllerAnimated(true)
+        }
+
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
+        
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImaageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
+        checkValidMealName()
     }
     
     // MARK: TextField  Delegate
@@ -32,7 +69,18 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
+        checkValidMealName()
+        navigationItem.title = textField.text
         
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        saveButton.enabled = false
+    }
+    
+    func checkValidMealName() {
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
     }
 
     @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
